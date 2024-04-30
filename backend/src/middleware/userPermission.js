531a425f6e0a0a10dd1d3d75ae8e5806603c647f
Tareq <elalitareq@gmail.com>
2024-04-30 17:38:53 +0300

    added user middleware

--- backend/src/middleware/userPermission.js
+++ backend/src/middleware/userPermission.js
@@ -1,12 +1,22 @@
-export async function isSuperAdmin(req, res, next) {
-    const { role } = req.body;
-    try {
-        if (role === "ADMIN" || role === "EMPLOYEE") {
-            return res.send({ message: "you are not a super admin" });
-        }
-        next();
-    } catch (err) {
-        next(err);
-        res.send({ message: err.message });
-    }
-}
+export const verifyToken = async (req, res, next) => {
+  const authorization = req.headers["authorization"].split(" ")[1];
+  if (!authorization) {
+    return res.send({ message: "Token is required" });
+  }
+
+  const token = authorization.split(" ")[1];
+  try {
+    const decoded = await jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.send({ message: "Invalid token" });
+  }
+};
+
+export const allowRoles = async (roles) => (req, res, next) => {
+  if (roles.includes(req.user.role)) {
+    return next();
+  }
+  return res.send({ message: "you are not allowed to access this resource" });
+};
