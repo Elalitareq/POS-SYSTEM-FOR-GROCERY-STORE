5672b80b4c7c4fcdac70455a4e0b977c4c63d195
Tareq <elalitareq@gmail.com>
2024-04-30 17:52:09 +0300

    fixed

--- backend/src/api/user.js
+++ backend/src/api/user.js
@@ -2,31 +2,26 @@
 import userDal from "../dal/user.js";
 import Express from "express";
 import {
-    removeUser,
-    addUser,
-    updateUser,
-    loginUser,
+  removeUser,
+  addUser,
+  updateUser,
+  loginUser,
 } from "../controllers/user.js";
 import { isSuperAdmin } from "../middleware/userPermission.js";
+import { tryCatch } from "../utils/functions.js";
 
 const router = Express.Router();
 
-router.get("/user", async (req, res) => {
-    try {
-        const users = await userDal.getAllUsers();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching users" });
-    }
-});
+router.get("/user", tryCatch(getAllUsers));
+
 router.get("/user/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const users = await userDal.getUserById(id);
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching users" });
-    }
+  const { id } = req.params;
+  try {
+    const users = await userDal.getUserById(id);
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
 });
 
 router.post("/create-user", addUser);
