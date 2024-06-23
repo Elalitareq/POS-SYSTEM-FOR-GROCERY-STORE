a5f26c031292aab25e67daf2a794bb87da3583bc
Tareq <elalitareq@gmail.com>
2024-06-23 15:04:52 +0300

    fixed

---
+++ backend/src/utils/userAtions.js
@@ -0,0 +1,17 @@
+const getObjectChangesInArabicAndEnglish = (oldObject, newObject) => {
+  const changes = [];
+  for (const key in newObject) {
+    if (oldObject[key] !== newObject[key]) {
+      changes.push({
+        key,
+        oldValue: oldObject[key],
+        newValue: newObject[key],
+      });
+    }
+  }
+
+  return {
+    ar: `${changes.map((change) => `${change.key} : ${change.oldValue} تم تغييرها الى ${change.newValue}`).join("\n")}`,
+    en: `${changes.map((change) => `${change.key} : ${change.oldValue} changed to ${change.newValue}`).join("\n")}`,
+  };
+};
